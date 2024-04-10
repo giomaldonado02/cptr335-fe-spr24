64961df02ea150d99ca98220ffac4b2999bab0e3
Nathan <nathangsweet@gmail.com>
2024-04-09 23:09:23 -0400

    Oops

--- stocks/src/router/index.js
+++ stocks/src/router/index.js
@@ -4,6 +4,7 @@ import LoginView from '../views/LoginView.vue'
 import UpdateUser from '../views/UpdateUser.vue'
 import LoggedIn from '../views/LoggedInView.vue'
 import Welcome from '../views/Welcome.vue'
+import ForgottenPasswordView from '../views/ForgottenPasswordView'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,6 +34,11 @@ const router = createRouter({
       name: 'welcome',
       component: Welcome
     },
+    {
+      path: '/forgotten',
+      name: 'forgotten',
+      component: ForgottenPasswordView
+    }
 
   ]
 })
